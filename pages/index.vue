2c29bc7005cd587f22e65d0e714365311addf4fb
paddy <niepeng@7ipr.com>
2019-03-01 10:31:50 +0800

    done

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="home-page">
     <svg-icon icon-class="user"/>
+    <hi-jsx/>
   </div>
 </template>
 
 <script>
+  import HiJsx from '~/components/hi-jsx'
   export default {
-
+    components: {HiJsx}
   };
 </script>
