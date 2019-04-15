430c9fbe807abe09cc4cfc414be71ddb88059fbb
paddy <niepeng@7ipr.com>
2019-04-15 22:53:34 +0800

    Todo List

--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,51 @@
   <div class="home-page">
     <svg-icon icon-class="user"/>
     <jsx/>
+
+    <el-form @submit.native.prevent="handleSubmit">
+      <el-input v-model="inputValue" placeholder="what needs to be finished?"></el-input>
+    </el-form>
+    <ul class="todo-list">
+      <li v-for="todo of todos" :key="todo.id">
+        <el-checkbox v-model="todo.finished" @change="handleChange(todo)">{{todo.title}}</el-checkbox>
+      </li>
+    </ul>
+    <div>{{left}} item(s) is unfinished!</div>
   </div>
 </template>
-
 <script>
   import jsx from '~/components/jsx'
 
   export default {
     components: {jsx},
-    async mounted() {
-
+    data() {
+      return {
+        inputValue: '',
+        todos: [],
+        done: 0
+      }
+    },
+    computed: {
+      left() {
+        return this.todos.length - this.done
+      }
+    },
+    methods: {
+      handleSubmit() {
+        this.todos.unshift({
+          id: Math.random(),
+          title: this.inputValue,
+          finished: false
+        })
+        this.inputValue = ''
+      },
+      handleChange(todo) {
+        if(todo.finished) {
+          this.done ++
+        }else {
+          this.done --
+        }
+      }
     }
   };
 </script>
