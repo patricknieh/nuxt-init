53b4c00b89a3cba85784563804d35f53f0df9b42
paddy <niepeng@7ipr.com>
2019-04-20 00:03:31 +0800

    utils

--- utils/index.js
+++ utils/index.js
@@ -5,21 +5,3 @@ export function sleep(time) {
     }, time)
   })
 }
-export function getPathName(path) {
-  switch (path) {
-    case '':
-      return '首页'
-      break
-    case 'admin':
-      return '仪表盘'
-      break
-    case 'articles':
-      return '文章管理'
-      break
-    case 'editor':
-      return '文章编辑'
-      break
-    default:
-      return ''
-  }
-}
