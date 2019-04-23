8ed586a7364641157a2da5ddc3fd8fccbcd6d77e
paddy <niepeng@7ipr.com>
2019-04-23 17:17:11 +0800

    socket.io-client

--- pages/index.vue
+++ pages/index.vue
@@ -16,13 +16,59 @@
 </template>
 <script>
   import jsx from '~/components/jsx'
+  import io from "socket.io-client"
 
   export default {
     components: {jsx},
+    mounted(){
+      const log = console.log;
+      const socket = io('http://localhost:7001', {
+
+        // 实际使用中可以在这里传递参数
+        query: {
+          room: 'demo',
+          userId: `client_${Math.random()}`,
+        },
+
+        transports: ['websocket']
+      });
+
+      socket.on('connect', () => {
+        const id = socket.id;
+
+        log('#connect,', id, socket);
+
+        // 监听自身 id 以实现 p2p 通讯
+        socket.on(id, msg => {
+          log('#receive,', msg);
+        });
+      });
+
+      // 接收在线用户信息
+      socket.on('online', msg => {
+        log('#online,', msg);
+      });
+
+      // 系统事件
+      socket.on('disconnect', msg => {
+        log('#disconnect', msg);
+      });
+
+      socket.on('disconnecting', () => {
+        log('#disconnecting');
+      });
+
+      socket.on('error', () => {
+        log('#error');
+      });
+
+      window.socket = socket
+    },
     data() {
       return {
         inputValue: '',
-        todos: []
+        todos: [],
+        socket: null
       }
     },
     computed: {
@@ -32,12 +78,22 @@
     },
     methods: {
       handleSubmit() {
+        //socket
+        window.socket.emit('exchange', {
+          target: '1UyxVUodyUUFGnoCAAAA',  //TODO change target id
+          payload: {
+            msg : this.inputValue,
+          },
+        });
+
         this.todos.unshift({
           id: Math.random(),
           title: this.inputValue,
           finished: false
         })
         this.inputValue = ''
+
+
       }
     }
   };
